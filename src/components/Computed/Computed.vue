fa1ead6215b03bb5eedaca84b067e07e5bc693ca
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-07 16:26:03 +0300

    computed property testing

---
+++ src/components/Computed/Computed.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 v-if="checkAge" data-test="warning">Yaşınız 18'den küçük olduğu için sistemimize kayıt işleminiz gerçekleştirilememiştir.</h1>
+    <h1 v-else>Kayıt başarılı bir şekilde gerçekleşmiştir.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComputedComponent',
+  props: {
+    age: {
+      type: Number,
+    },
+  },
+  computed: {
+    checkAge() {
+      return this.age < 18;
+    }
+  },
+};
+</script>
---
+++ tests/unit/computedProperties.spec.js
@@ -0,0 +1,19 @@
+import { mount } from "@vue/test-utils"
+import Computed from "../../src/components/Computed/Computed";
+
+describe("Computed Properties Testing", () => {
+    const factory = (options) => {
+        return mount(Computed, {
+            propsData: {
+                ...options
+            }
+        })
+    }
+    test("should render user form",() => {
+        const params = {age: 17}
+        const wrapper = factory(params)
+        expect(wrapper.vm.checkAge).toBeTruthy()
+        const warning = wrapper.find("[data-test=\"warning\"]")
+        expect(warning.text()).toEqual('Yaşınız 18\'den küçük olduğu için sistemimize kayıt işleminiz gerçekleştirilememiştir.')
+    })
+})
